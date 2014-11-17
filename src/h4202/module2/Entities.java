f3c44dd1d328995dff6dfb203eee4a95f99b6eea
Alexandre Sarazin <alexandre.sarazin1@gmail.com>
2014-11-17 10:59:53 +0100

    Added sample2 + Modified sample 1 + worked on Entities.java

--- src/h4202/module2/Entities.java
+++ src/h4202/module2/Entities.java
@@ -1,8 +1,41 @@
 package h4202.module2;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Entities {
+	public static void main(String[] args) {
+		
+		HashMap<String, String> urlTexts = new HashMap<String, String>();
+		
+		JSONParser parser = new JSONParser();
+		try {
+			FileReader filereader = new FileReader(new File("part1_sample.json"));
+			
+			JSONObject obj = (JSONObject) parser.parse(filereader);
+			JSONArray pages = (JSONArray) obj.get("pages");
+			
+			for(int i=0; i < pages.size(); i++){
+				JSONObject doublet = (JSONObject)pages.get(i);
+				urlTexts.put(doublet.get("link").toString(), doublet.get("text").toString());
+			}
+			
+		} catch (IOException ie) {
+			System.out.println("File error : not found");
+		} catch (ParseException pe) {
+			System.out.println("position: " + pe.getPosition());
+			System.out.println(pe);
+		} 
+		
+		
+	}
 	
-	JSONObject test;
+	
 }
