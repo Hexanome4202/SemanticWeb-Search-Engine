26554b8872ae5b649280527841227f3e5703a867
Rémi Martin <mrmi11@yahoo.fr>
2014-11-17 10:48:31 +0100

    Update GoogleResults.java

--- src/h4202/GoogleResults.java
+++ src/h4202/GoogleResults.java
@@ -6,17 +6,26 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class GoogleResults {
 	private static String API_KEY = "AIzaSyD4vwwD8HzF1NLbhE04U0td5NOmH1NYwpw";
 	private static String URL_QUERY = "https://www.googleapis.com/customsearch/v1";
 	
 	public static void main(String[] args) {
-		search("obama");
+		System.out.println(getElements(search("obama")));
 	}
 	
-	public static void search(String keywords) {
+	public static String search(String keywords) {
 		URL url;
+		String text = "";
 		 
 		try {
 			// get URL content
@@ -29,7 +38,7 @@ public class GoogleResults {
 			String inputLine;
  
 			while ((inputLine = br.readLine()) != null) {
-				System.out.println(inputLine);
+				text += inputLine;
 			}
 			br.close();
  
@@ -38,6 +47,40 @@ public class GoogleResults {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
- 
+		
+		return text;
+	}
+	
+	/**
+	 * 
+	 * @param json
+	 * @return a Google element created with the html
+	 */
+	public static List<GoogleElement> getElements(String json) {
+		JSONParser parser = new JSONParser();
+		List<GoogleElement> elements = new ArrayList<GoogleElement>();
+		
+		Object obj;
+		try {
+			obj = parser.parse(json);
+			JSONObject jsonObject = (JSONObject) obj;
+			 
+			// loop array
+			JSONArray links = (JSONArray) jsonObject.get("items");
+			Iterator<JSONObject> iterator = links.iterator();
+			while (iterator.hasNext()) {
+				elements.add(new GoogleElement(iterator.next().get("link").toString(), ""));
+			}
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return elements;
+	}
+	
+	public static String getTextFromPage(String link) {
+		return "";
 	}
 }
