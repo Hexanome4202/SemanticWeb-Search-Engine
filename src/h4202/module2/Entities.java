0e6df0f43c0bec3f863679ebb8ea0260537843cb
alsarazin <alexandre.sarazin1@gmail.com>
2014-11-22 17:07:00 +0100

    getGraph() method is working
    Added comments, added getGraph() method, finished the creation of
    triplets

--- src/h4202/module2/Entities.java
+++ src/h4202/module2/Entities.java
@@ -9,7 +9,6 @@ import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.HashMap;
@@ -25,23 +24,16 @@ import org.json.simple.parser.ParseException;
 public class Entities {
 	public static void main(String[] args) {
 		
-		HashMap<String, Set<String>> entities = new HashMap<String, Set<String>>();
+		HashMap<String, Set<Triplet>> url_triplets = new HashMap<String, Set<Triplet>>();
 		
+		// Parsing a JSON file -> WILL NOT BE USED AFTER
 		JSONParser parser = new JSONParser();
 		try {
 			FileReader filereader = new FileReader(new File("part1_sample.json"));
 			
 			JSONObject obj = (JSONObject) parser.parse(filereader);
-			JSONArray pages = (JSONArray) obj.get("pages");
 			
-			for(int i=0; i < pages.size(); i++){
-				JSONObject doublet = (JSONObject)pages.get(i);
-				
-				Set<String> uris = getEntities(doublet.get("text").toString());				
-				entities.put(doublet.get("link").toString(), uris);
-				
-				getTriplets(uris);
-			}
+			url_triplets = getGraph(obj);
 			
 		} catch (IOException ie) {
 			System.out.println(ie);
@@ -50,11 +42,45 @@ public class Entities {
 		}
 	}
 	
+	/**
+	 * Method that with a JSONObject, returns a Graph of URL/Triplets
+	 * @param object : the JSONObject containing all the pages' URL
+	 * @return the graph as a HashMap<url,triplets>
+	 */
+	public static HashMap<String, Set<Triplet>> getGraph(JSONObject object){
+		
+		HashMap<String, Set<Triplet>> url_triplets = new HashMap<String, Set<Triplet>>();
+		
+		JSONArray pages = (JSONArray) object.get("pages");
+		
+		for(int i=0; i < pages.size(); i++){
+			
+			JSONObject doublet = (JSONObject)pages.get(i);
+		
+			String url = doublet.get("link").toString();
+			
+			Set<String> uris = getEntities(doublet.get("text").toString());				
+			
+			Set<Triplet> triplets = getTriplets(uris);
+			
+			url_triplets.put(url, triplets);
+		}
+		
+		return url_triplets;
+	}
+
+	/**
+	 * Method that gives all the triplets (subject, predicate, object) found for a set of subject URI's  
+	 * @param resources : set of URI's 
+	 * @return Set<Triplet> : all the triplets found (subject = URI given)
+	 */
 	public static Set<Triplet> getTriplets(Set<String> resources) {
 		Set<Triplet> triplets = new HashSet<Triplet>();		 
 				
 		String uris = "";
 		Iterator<String> it = resources.iterator();
+		
+		// for each URI given, we encode it 
 		while(it.hasNext()){
 			
 			try {
@@ -62,15 +88,14 @@ public class Entities {
 				if(it.hasNext()) 
 					uris += "%2C";
 			} catch (UnsupportedEncodingException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 
 		}
 		
-		
+		// run a sparql query to get the triplets
 		try {
-			String spotlightResult = "";
+			String sparqlResult = "";
 			
 			URI uri = new URI("http://live.dbpedia.org/sparql" +
 					"?query=SELECT+*+WHERE+%7B+%3Fs+%3Fp+%3Fo.+FILTER%28%3Fs+in+%28"
@@ -85,22 +110,30 @@ public class Entities {
 			// open the stream and put it into BufferedReader
 			BufferedReader br = new BufferedReader(
                                new InputStreamReader(conn.getInputStream()));
+			
 			String inputLine;
- 
 			while ((inputLine = br.readLine()) != null) {
-				spotlightResult += inputLine;
+				sparqlResult += inputLine;
 			}
 			br.close();
 			
-			System.out.println(spotlightResult);
+			JSONParser parser = new JSONParser();
 			
-			/*JSONParser parser = new JSONParser();
+			// parse the string as a JSON Object
+			JSONObject obj = (JSONObject) parser.parse(sparqlResult);
+			// get all the triplets
+			JSONArray bindings = (JSONArray) ((JSONObject) obj.get("results")).get("bindings");
 			
-			JSONObject obj = (JSONObject) parser.parse(spotlightResult);
-			JSONArray resources = (JSONArray) obj.get("Resources");
-			for(int i=0; i < resources.size(); i++){
-				result.add((((JSONObject)resources.get(i)).get("@URI")).toString());
-			}*/
+			// for each triplet, create a new Triplet Object
+			for(int i=0; i < bindings.size(); i++){
+				
+				String subject = ((JSONObject)((JSONObject)bindings.get(i)).get("s")).get("value").toString();
+				String predicate = ((JSONObject)((JSONObject)bindings.get(i)).get("p")).get("value").toString();
+				String object = ((JSONObject)((JSONObject)bindings.get(i)).get("o")).get("value").toString();
+				
+				Triplet triplet = new Triplet(subject,predicate,object);
+				triplets.add(triplet);
+			}
  
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
@@ -108,11 +141,18 @@ public class Entities {
 			e.printStackTrace();
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
 		}
 		
-		return null;
+		return triplets;
 	}
 	
+	/**
+	 * Method that calls DBPedia Spotlight with a text to get the entities inside 
+	 * @param text : the text to analyze
+	 * @return Set<String> : a set of all the entities found
+	 */
 	public static Set<String> getEntities(String text){
 		Set<String> result = new HashSet<String>();
 		
@@ -123,22 +163,23 @@ public class Entities {
 		try {
 			String spotlightResult = "";
 			URI uri = new URI("http","//spotlight.dbpedia.org/rest/annotate?text="+text+params,null);
-			// get URL content
+			// get URL content in JSON
 			URLConnection conn = uri.toURL().openConnection();
 			conn.setRequestProperty("Accept", "application/json");
  
 			// open the stream and put it into BufferedReader
 			BufferedReader br = new BufferedReader(
                                new InputStreamReader(conn.getInputStream()));
+
+			// get all the json
 			String inputLine;
- 
 			while ((inputLine = br.readLine()) != null) {
 				spotlightResult += inputLine;
 			}
 			br.close();
 			
+			// parsing the json to put in the set only the URI's
 			JSONParser parser = new JSONParser();
-			
 			JSONObject obj = (JSONObject) parser.parse(spotlightResult);
 			JSONArray resources = (JSONArray) obj.get("Resources");
 			for(int i=0; i < resources.size(); i++){
