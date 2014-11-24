0fecd52b5cfff0d3f51ba3158349b1d9e80a7f07
Cécilia Van Bever <ceciliavanbever@gmail.com>
2014-11-24 10:56:32 +0100

    Removed outputs

--- src/h4202/GoogleResults.java
+++ src/h4202/GoogleResults.java
@@ -39,6 +39,10 @@ public class GoogleResults {
 	 * @return
 	 */
 	public static String search(String keywords, int pageNum) {
+<<<<<<< Updated upstream
+=======
+		System.out.println("DŽbut search");
+>>>>>>> Stashed changes
 		URL url;
 		String text = "";
 		int startIndex = (pageNum - 1) * NB_RESULTS + 1;
